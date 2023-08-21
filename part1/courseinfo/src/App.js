42f7141789398aa42b1d8f5bb1b52de4bb97caca
lerkooow <lerkoooow@gmail.com>
2023-08-21 18:31:59 +0700

    step1

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,67 +1,37 @@
+import { useState } from 'react'
+
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleGoodClick = () => {
+    setGood(good + 1)
   }
-
-
-  const Header = ({ course }) => {
-    return (
-      <div>
-        <h1>{course}</h1>
-      </div>
-    );
-  };
-
-  const Part = ({ part, exercises }) => {
-    return (
-      <div>
-        <p>
-          {part} {exercises}
-        </p>
-      </div>
-    );
-  };
-
-  const Content = ({ course }) => {
-
-    return (
-      <div>
-        <Part part={course.parts[0].name} exercises={course.parts[0].exercises} />
-        <Part part={course.parts[1].name} exercises={course.parts[1].exercises} />
-        <Part part={course.parts[2].name} exercises={course.parts[2].exercises} />
-      </div>
-    );
-  };
-
-  const Total = ({ course }) => {
-    return (
-      <div>
-        <p>Number of exercises {course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises}</p>
-      </div>
-    );
-  };
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1)
+  }
+  const handleBadClick = () => {
+    setBad(bad + 1)
+  }
 
   return (
     <div>
-      <Header course={course.name} />
-      <Content course={course} />
-      <Total course={course} />
-    </div>
-  );
-};
+      <h2>give feetback</h2>
 
-export default App;
+      <button onClick={handleGoodClick}>good</button>
+      <button onClick={handleNeutralClick}>neutral</button>
+      <button onClick={handleBadClick}>bad</button>
+
+      <h2>statistic</h2>
+
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+
+    </div>
+  )
+}
+
+export default App
