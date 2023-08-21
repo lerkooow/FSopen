4aab1455b9432882a190344c98eb5aa93602f7fb
lerkooow <lerkoooow@gmail.com>
2023-08-21 19:02:10 +0700

    step5

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,29 @@
-import { useState } from 'react'
+import { useState } from 'react';
+
+const Filter = ({ filter, handleFilterChange }) => {
+  return (
+    <div>filter shown with <input value={filter} onChange={handleFilterChange} /></div>
+  );
+};
+
+const PersonForm = ({ newName, newNumber, handleNameChange, handleNumberChange }) => {
+  return (
+    <div>
+      name: <input value={newName} onChange={handleNameChange} /><br />
+      number: <input value={newNumber} onChange={handleNumberChange} />
+    </div>
+  )
+}
+
+const Persons = ({ filteredPersons }) => {
+  return (
+    <div>
+      {filteredPersons.map(person => (
+        <p key={person.id}>{person.name} {person.number}</p>
+      ))}
+    </div>
+  )
+}
 
 const App = () => {
 
@@ -7,54 +32,54 @@ const App = () => {
     { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
     { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
     { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  ]);
 
-  const [newName, setNewName] = useState('')
-  const [newNumber, setNewNumber] = useState('')
-  const [filter, setFilter] = useState("")
+  const [newName, setNewName] = useState('');
+  const [newNumber, setNewNumber] = useState('');
+  const [filter, setFilter] = useState('');
 
+  // Name
+
+  const handleNameChange = (e) => {
+    setNewName(e.target.value);
+  };
+
+  // Number
+
+  const handleNumberChange = (e) => {
+    setNewNumber(e.target.value);
+  };
+
+  // Add Name and Number 
   const addNameNumber = (e) => {
-    e.preventDefault()
+    e.preventDefault();
 
-    // Input validation
-    if (newName === "" && newNumber === "") {
-      alert("Enter Data")
-      return
-    } else if (newName === "") {
-      alert("Enter Name")
-      return
-    } else if (newNumber === "") {
-      alert("Enter Number")
-      return
+    if (!newName || !newNumber) {
+      alert("Please enter both name and number");
+      return;
+    }
+
+    if (persons.some(person => person.name === newName)) {
+      alert(`${newName} is already added to phonebook`);
+      return;
     }
 
     const nameObject = {
       name: newName,
       number: newNumber,
       id: persons.length + 1
-    }
+    };
 
-    if (persons.some(person => person.name === newName)) {
-      alert(`${newName} is already added to phonebook`)
-      return
-    }
+    setPersons(persons.concat(nameObject));
+    setNewName('');
+    setNewNumber('');
+  };
 
-    setPersons(persons.concat(nameObject))
-    setNewName("")
-    setNewNumber("")
-  }
-
-  const handleNameChange = (e) => {
-    setNewName(e.target.value)
-  }
-
-  const handleNumberChange = (e) => {
-    setNewNumber(e.target.value)
-  }
+  // Filter
 
   const handleFilterChange = (e) => {
-    setFilter(e.target.value)
-  }
+    setFilter(e.target.value);
+  };
 
   const filteredPersons = persons.filter(person =>
     person.name.toLowerCase().includes(filter.toLowerCase())
@@ -63,23 +88,20 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
-      <div>filter shown with <input value={filter} onChange={handleFilterChange} /></div>
+      <Filter filter={filter} handleFilterChange={handleFilterChange} />
       <form onSubmit={addNameNumber}>
         <div>
-          <h2>add a new</h2>
-          name: <input value={newName} onChange={handleNameChange} />
-          <div>number: <input value={newNumber} onChange={handleNumberChange} /></div>
+          <h3>Add a new</h3>
+          <PersonForm newName={newName} newNumber={newNumber} handleNameChange={handleNameChange} handleNumberChange={handleNumberChange} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {filteredPersons.map(person => (
-        <p key={person.id}>{person.name} {person.number}</p>
-      ))}
+      <Persons filteredPersons={filteredPersons} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
