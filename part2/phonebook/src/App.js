563c801df5e6114d35a56105e086a9ade685e6ff
lerkooow <lerkoooow@gmail.com>
2023-08-21 19:01:26 +0700

    step3

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,41 +2,66 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    {
+      name: 'Arto Hellas',
+      number: "040-1234567"
+    }
   ])
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
-  const addName = (e) => {
+  const addNameNumber = (e) => {
     e.preventDefault()
+
+    // Input validation
+    if (newName === "" && newNumber === "") {
+      alert("Enter Data")
+      return
+    } else if (newName === "") {
+      alert("Enter Name")
+      return
+    } else if (newNumber === "") {
+      alert("Enter Number")
+      return
+    }
+
     const nameObject = {
       name: newName,
+      number: newNumber
     }
-    // Метод some() проверяет, удовлетворяет ли какой-либо элемент массива условию, заданному в передаваемой функции.
+
     if (persons.some(person => person.name === newName)) {
       alert(`${newName} is already added to phonebook`)
       return
     }
+
     setPersons(persons.concat(nameObject))
-    setNewName("") //Сброс поля ввода после отправки имени
+    setNewName("")
+    setNewNumber("")
   }
 
   const handleNameChange = (e) => {
     setNewName(e.target.value)
   }
 
+  const handleNumberChange = (e) => {
+    setNewNumber(e.target.value)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={addName}>
+      <form onSubmit={addNameNumber}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
+          <div>number: <input value={newNumber} onChange={handleNumberChange} /></div>
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      <div>{persons.map(person => <p key={person.name}>{person.name}</p>)}</div>
+      <div>{persons.map(person => <p key={person.name}>{person.name} {person.number}</p>)}</div>
     </div>
   )
 }
