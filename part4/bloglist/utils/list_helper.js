7226e1eb2b9db87332553696b5384e97f29dd7cf
lerkooow <lerkoooow@gmail.com>
2023-09-05 16:40:22 +0700

    4.5*: helper functions and unit tests, step3

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -8,7 +8,22 @@ const totalLikes = (blogs) => {
     return likes
 }
 
+const favoriteBlog = (blogs) => {
+    if (blogs.length === 0) {
+        return 0
+    }
+
+    const favorite = blogs.reduce((fav, blog) => (fav.likes > blog.likes ? fav : blog))
+
+    return {
+        title: favorite.title,
+        author: favorite.author,
+        likes: favorite.likes,
+    }
+}
+
 module.exports = {
     dummy,
     totalLikes,
+    favoriteBlog
 }
