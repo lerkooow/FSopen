7cd0cc67e64de88acae30d05d703c9732a1bafaa
lerkooow <lerkoooow@gmail.com>
2023-09-05 15:58:18 +0700

    4.4: helper functions and unit tests, step2

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -2,6 +2,13 @@ const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = (blogs) => {
+    const likes = blogs.length === 0 ? 0 : blogs.reduce((sum, blog) => sum + blog.likes, 0)
+
+    return likes
+}
+
 module.exports = {
     dummy,
+    totalLikes,
 }
